78b7dd89ed7bc770d6d2ea110dbe3dbfcb76b061
kwesman <paakwasisensor@gmail.com>
2023-05-22 11:37:44 -0700

    broken our homepage into smaller components, finished styling our menu-item component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -10,28 +10,28 @@ class Directory extends React.Component {
         this.state = {
             sections:[{
                 title: 'hats',
-                imageurl: 'https://i.ibb.co/cvpntL1/hats.png',
+                imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                 id: 1
             },
             {
                 title: 'jackets',
-                imageurl: 'https://i.ibb.co/px2tCc3/jackets.png',
+                imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
                 id: 2
             },
             {
                 title: 'sneakers',
-                imageurl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+                imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
                 id: 3
             },
             {
                 title: 'women',
-                imageurl: 'https://i.ibb.co/GCCdy8t/womens.png',
+                imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
                 size: 'large',
                 id: 4
             },
             {
                 title: 'men',
-                imageurl: 'https://i.ibb.co/R70vBrQ/men.png',
+                imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
                 size: 'large',
                 id: 5
             }
